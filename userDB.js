e3e4e6cb4186b54896daa873e0aaca2513af0960
Enbe Zhou <enbe.zhou@skyscanner.net>
2021-01-26 05:08:09 +0800

    map video chat.

--- userDB.js
+++ userDB.js
@@ -1,23 +1,6 @@
 
 userDB = {
-    userList:[
-        {
-            "uuid": "ea67225832",
-            "userIcon": 1,
-            "lng": 113.94028,
-            "lat": 22.5059
-        }, {
-            "uuid": "8a6722583a",
-            "userIcon": 2,
-            "lng": 113.94528,
-            "lat": 22.5059
-        }, {
-            "uuid": "2a67225839",
-            "userIcon": 3,
-            "lng": 113.95028,
-            "lat": 22.5059
-        }
-    ],
+    userList:[],
     userSocketList: new Map()
 }
 
